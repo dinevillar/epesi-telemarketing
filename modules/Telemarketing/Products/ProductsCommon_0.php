d66ac0ae6571d720cc3959602d2a659b536ebbc2
Dine Villar <dine@multisoft.com>
2020-02-11 16:43:40 +0800

    Added optional products submodule

---
+++ modules/Telemarketing/Products/ProductsCommon_0.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @Author: Rodine Mark Paul L. Villar <dean.villar@gmail.com>
+ * @Date: 2/11/20
+ * @Time: 1:00 PM
+ */
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+
+/**
+ * Class Telemarketing_ProductsCommon
+ */
+class Telemarketing_ProductsCommon extends ModuleCommon
+{
+    /**
+     * @return array
+     */
+    public static function menu()
+    {
+        return array(
+            _M('Telemarketing') =>
+                array('__submenu__' => 1, _M('Products') => array('__weight__' => 1))
+        );
+    }
+
+    /**
+     * @param null $rid
+     * @param array $events
+     * @param bool $details
+     * @return array|null
+     */
+    public function product_watchdog_label($rid = NULL, $events = array(), $details = TRUE)
+    {
+        return Utils_RecordBrowserCommon::watchdog_label(
+            Telemarketing_Products_RBO_Products::TABLE_NAME, __('Products'), $rid, $events,
+            array('ContactedBase_Products_RBO_ProductItems', 'display_name'), $details
+        );
+    }
+
+    /**
+     * @param $values
+     * @param $mode
+     * @return mixed
+     */
+    public static function submit_products($values, $mode)
+    {
+        if ($mode == "adding") {
+            $values['permission'] = Base_User_SettingsCommon::get(
+                'CRM_Common',
+                'default_record_permission'
+            );
+        }
+        return $values;
+    }
+
+}
---
+++ modules/Telemarketing/Products/ProductsInstall.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @Author: Rodine Mark Paul L. Villar <dean.villar@gmail.com>
+ * @Date: 2/10/20
+ * @Time: 2:10 AM
+ */
+defined("_VALID_ACCESS") || die();
+
+
+class Telemarketing_ProductsInstall extends ModuleInstall
+{
+
+    /**
+     * Module installation function.
+     * @return true if installation success, false otherwise
+     */
+    public function install()
+    {
+        Base_ThemeCommon::install_default_theme(self::module_name());
+        $products_rbo = new Telemarketing_Products_RBO_Products();
+        if ($products_rbo->install()) {
+            $products_rbo->set_quickjump(__('Name'));
+            $products_rbo->set_favorites(TRUE);
+            $products_rbo->set_recent(15);
+            $products_rbo->set_caption(__("Products"));
+            $products_rbo->set_icon(
+                Base_ThemeCommon::get_template_filename(
+                    self::module_name(), 'icon.png')
+            );
+            Utils_RecordBrowserCommon::enable_watchdog(
+                Telemarketing_Products_RBO_Products::TABLE_NAME,
+                array('Telemarketing_ProductsCommon', 'product_watchdog_label')
+            );
+            self::install_permissions();
+            Utils_AttachmentCommon::new_addon(Telemarketing_Products_RBO_Products::TABLE_NAME);
+
+            Utils_RecordBrowserCommon::register_processing_callback(
+                Telemarketing_Products_RBO_Products::TABLE_NAME,
+                array('Telemarketing_ProductsCommon', 'submit_products'));
+            return true;
+        }
+    }
+
+    public static function install_permissions()
+    {
+        $tab = Telemarketing_Products_RBO_Products::TABLE_NAME;
+        Utils_RecordBrowserCommon::wipe_access($tab);
+        Utils_RecordBrowserCommon::add_access($tab, 'print', 'SUPERADMIN');
+        Utils_RecordBrowserCommon::add_access($tab, 'export', 'SUPERADMIN');
+        Utils_RecordBrowserCommon::add_access($tab, 'view', 'ACCESS:employee', array('(!permission' => 2, '|:Created_by' => 'USER_ID'));
+        Utils_RecordBrowserCommon::add_access($tab, 'view', 'ALL', array('id' => 'USER_COMPANY'));
+        Utils_RecordBrowserCommon::add_access($tab, 'add', 'ACCESS:employee');
+        Utils_RecordBrowserCommon::add_access($tab, 'edit', 'ACCESS:employee', array('(permission' => 0, '|:Created_by' => 'USER_ID'));
+        Utils_RecordBrowserCommon::add_access($tab, 'edit', array('ALL', 'ACCESS:manager'), array('id' => 'USER_COMPANY'));
+        Utils_RecordBrowserCommon::add_access($tab, 'edit', array('ACCESS:employee', 'ACCESS:manager'), array());
+        Utils_RecordBrowserCommon::add_access($tab, 'delete', 'ACCESS:employee', array(':Created_by' => 'USER_ID'));
+        Utils_RecordBrowserCommon::add_access($tab, 'delete', array('ACCESS:employee', 'ACCESS:manager'));
+    }
+
+    /**
+     * Module uninstallation function.
+     * @return true if installation success, false otherwise
+     */
+    public function uninstall()
+    {
+        $products_rbo = new Telemarketing_Products_RBO_Products();
+        if ($products_rbo->uninstall()) {
+            Base_ThemeCommon::uninstall_default_theme(self::module_name());
+            Utils_AttachmentCommon::delete_addon(Telemarketing_Products_RBO_Products::TABLE_NAME);
+            Utils_RecordBrowserCommon::unregister_processing_callback(
+                Telemarketing_Products_RBO_Products::TABLE_NAME,
+                array('Telemarketing_ProductsCommon', 'submit_products')
+            );
+            return true;
+        }
+    }
+
+    /**
+     * Returns array that contains information about modules required by this module.
+     * The array should be determined by the version number that is given as parameter.
+     *
+     * @param int $v module version number
+     * @return array Array constructed as following: array(array('name'=>$ModuleName,'version'=>$ModuleVersion),...)
+     */
+    public function requires($v)
+    {
+        return array(
+            array('name' => CRM_ContactsInstall::module_name(), 'version' => 0)
+        );
+    }
+
+    public function simple_setup()
+    {
+        return array(
+            'package' => __('Telemarketing'),
+            'option' => __('Products')
+        );
+    }
+
+    public function version()
+    {
+        return array("0.1");
+    }
+
+    public function info()
+    {
+        return array(
+            'Author' => '<a href="mailto:dean.villar@gmail.com">Rodine Mark Paul Villar</a>',
+            'Description' => "Products"
+        );
+    }
+
+}
