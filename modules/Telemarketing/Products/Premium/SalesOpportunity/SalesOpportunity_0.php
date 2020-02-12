3f1c411f28cc9b4db09809fcb74db0f372b99ea5
Dine Villar <dine@multisoft.com>
2020-02-12 20:10:10 +0800

    Added sales opportunity integration

---
+++ modules/Telemarketing/Products/Premium/SalesOpportunity/SalesOpportunity_0.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @Author: Rodine Mark Paul L. Villar <dean.villar@gmail.com>
+ * @Date: 2/11/20
+ * @Time: 4:53 PM
+ */
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+
+class Telemarketing_Products_Premium_SalesOpportunity extends Module
+{
+    public function sales_opportunities_addon($arg)
+    {
+        $rb = $this->init_module('Utils/RecordBrowser', 'premium_salesopportunity', 'premium_salesopportunity');
+        $params = array(
+            array('product' => $arg['id']),
+            array('product' => false),
+            array('id' => 'DESC')
+        );
+
+        $me = CRM_ContactsCommon::get_my_record();
+        $rb->set_defaults(
+            array(
+                'product' => $arg['id'],
+                'opportunity_manager' => $me['id'],
+                'employees' => $me['id'],
+                'start_date' => date('Y-m-d'), 'status' => 0)
+        );
+        $this->display_module($rb, $params, 'show_data');
+    }
+}
