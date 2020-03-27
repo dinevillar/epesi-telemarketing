67a7e8ec40ee718d2e3097ee72a4fd889f30082e
Dine Villar <dine@multisoft.com>
2020-03-28 04:32:47 +0800

    Added starts_with and ends_with in misc

--- include/misc.php
+++ include/misc.php
@@ -126,7 +126,7 @@ function on_exit($u = null, $args = null, $stable=true, $ret = false, $last_call
 		if($last_call) {
 			$ret = $headers;
 			$headers = array();
-		} else { 
+		} else {
 			$ret = array();
 			foreach($headers as $v)
 				if($v['stable']) $ret[] = $v; //if stable call always
@@ -200,7 +200,7 @@ function dir_tree($path, $hidden=false, $maxdepth = -1, $d = 0) {
 	$dirlist[] = $path;
 	if ($handle = opendir($path)) {
 		while (false !== ($file = readdir($handle))) {
-			if ($file == '.' || $file == '..' || (!$show_hidden && preg_match('/^\./',$file)) || ($hiddens && in_array($file,$hiddens))) 
+			if ($file == '.' || $file == '..' || (!$show_hidden && preg_match('/^\./',$file)) || ($hiddens && in_array($file,$hiddens)))
 				continue;
 			$file = $path . $file;
 			if (is_dir($file) && $d >= 0 && ($d < $maxdepth || $maxdepth < 0)) {
@@ -390,7 +390,7 @@ function create_html_form(& $form_name, $action_url, $variables, $target = null,
 // mobile devices
 
 function detect_mobile_device(){
-  
+
   // check if the user agent value claims to be windows but not windows mobile
   if(stristr($_SERVER['HTTP_USER_AGENT'],'windows')&&!(stristr($_SERVER['HTTP_USER_AGENT'],'windows ce')||stristr($_SERVER['HTTP_USER_AGENT'],'windows mobile')||stristr($_SERVER['HTTP_USER_AGENT'],'palm'))){
     return false;
@@ -536,7 +536,7 @@ class EpesiHTML {
 		if($name=='EPESI' || $name=='BR') return;
 		echo "</$name>";
 	}
-	
+
 	public static function characterData($parser, $data) {
 		echo $data;
 	}
@@ -667,49 +667,49 @@ class EpesiHTML {
 	}
 }
 
-function curl_exec_follow($ch, $maxredirect = null) { 
-	$mr = $maxredirect === null ? 5 : intval($maxredirect); 
-	if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) { 
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0); 
-		curl_setopt($ch, CURLOPT_MAXREDIRS, $mr); 
-	} else { 
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); 
-		if ($mr > 0) { 
-			$newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+function curl_exec_follow($ch, $maxredirect = null) {
+	$mr = $maxredirect === null ? 5 : intval($maxredirect);
+	if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+	} else {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		if ($mr > 0) {
+			$newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
 
-			$rch = curl_copy_handle($ch); 
-			curl_setopt($rch, CURLOPT_HEADER, true); 
-			curl_setopt($rch, CURLOPT_NOBODY, true); 
-			curl_setopt($rch, CURLOPT_FORBID_REUSE, false); 
-			curl_setopt($rch, CURLOPT_RETURNTRANSFER, true); 
-			do { 
-				curl_setopt($rch, CURLOPT_URL, $newurl); 
-				$header = curl_exec($rch); 
-				if (curl_errno($rch)) { 
-					$code = 0; 
-				} else { 
-					$code = curl_getinfo($rch, CURLINFO_HTTP_CODE); 
-					if ($code == 301 || $code == 302) { 
-						preg_match('/Location:(.*?)\n/', $header, $matches); 
-						$newurl = trim(array_pop($matches)); 
-					} else { 
-						$code = 0; 
-					} 
-				} 
-			} while ($code && --$mr); 
-			curl_close($rch); 
-			if (!$mr) { 
-				if ($maxredirect === null) { 
-					trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING); 
-				} else { 
-					$maxredirect = 0; 
-				} 
-				return false; 
-			} 
-			curl_setopt($ch, CURLOPT_URL, $newurl); 
-		} 
-	} 
-	return curl_exec($ch); 
+			$rch = curl_copy_handle($ch);
+			curl_setopt($rch, CURLOPT_HEADER, true);
+			curl_setopt($rch, CURLOPT_NOBODY, true);
+			curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+			curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
+			do {
+				curl_setopt($rch, CURLOPT_URL, $newurl);
+				$header = curl_exec($rch);
+				if (curl_errno($rch)) {
+					$code = 0;
+				} else {
+					$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+					if ($code == 301 || $code == 302) {
+						preg_match('/Location:(.*?)\n/', $header, $matches);
+						$newurl = trim(array_pop($matches));
+					} else {
+						$code = 0;
+					}
+				}
+			} while ($code && --$mr);
+			curl_close($rch);
+			if (!$mr) {
+				if ($maxredirect === null) {
+					trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING);
+				} else {
+					$maxredirect = 0;
+				}
+				return false;
+			}
+			curl_setopt($ch, CURLOPT_URL, $newurl);
+		}
+	}
+	return curl_exec($ch);
 }
 
 function get_function_caller($describe=true) {
@@ -735,4 +735,24 @@ function get_function_caller($describe=true) {
 	}
 
 	return $ret;
+}
+
+if (!function_exists('starts_with')) {
+    function starts_with($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+}
+
+if (!function_exists('ends_with')) {
+    function ends_with($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+
+        return (substr($haystack, -$length) === $needle);
+    }
 }
